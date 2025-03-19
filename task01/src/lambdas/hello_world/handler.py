f33f93cbd0eb822f5c3c92846640ff894743a1f8
Tetiana Trukhina <tetyana.trukhina@gmail.com>
2025-03-20 01:59:12 +0200

    Correct return message

--- task01/src/lambdas/hello_world/handler.py
+++ task01/src/lambdas/hello_world/handler.py
@@ -1,5 +1,6 @@
 from commons.log_helper import get_logger
 from commons.abstract_lambda import AbstractLambda
+import json
 
 _LOG = get_logger(__name__)
 
@@ -14,7 +15,15 @@ class HelloWorld(AbstractLambda):
         Explain incoming event here
         """
         # todo implement business logic
-        return 200, {"message": "Hello from Lambda"}
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({
+                "message": "Hello from Lambda"
+            })
+        }
     
 
 HANDLER = HelloWorld()
