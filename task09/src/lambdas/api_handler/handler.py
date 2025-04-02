72b80bddf2e29a468d6309b69366579e68b8b58d
Tetiana Trukhina <tetyana.trukhina@gmail.com>
2025-04-02 19:58:34 +0300

    Edit response

--- task09/src/lambdas/api_handler/handler.py
+++ task09/src/lambdas/api_handler/handler.py
@@ -14,14 +14,25 @@ class ApiHandler(AbstractLambda):
         pass
         
     def handle_request(self, event, context):
+        http = event['requestContext']['http']
+        path = http['path']
+        method = http['method']
+
         URL = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m'
         response = requests.get(URL)
-        # return 200
-        # client = OpenMeteoClient(URL)
-        # response = client.get_current_weather()
+
+        if path == '/weather':
+            return {
+                "statusCode": 200,
+                "body": json.dumps(response.json())
+            }
+
         return {
-            "statusCode": 200,
-            "body": json.dumps(response.json())
+            "statusCode": 400,
+            "body": json.dumps({
+                "statusCode": 400,
+                "message": f"Bad Request syntax or unsupported method. Request path: {path}. HTTP method: {method}"
+            })
         }
 
 HANDLER = ApiHandler()
